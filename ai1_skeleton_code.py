164d84e975fee7afa5ac9e40ce804a6f1fb77023
eggtartytq <50469264+eggtartytq@users.noreply.github.com>
2022-10-05 02:38:26 -0700

    Add files via upload

--- ai1_skeleton_code.py
+++ ai1_skeleton_code.py
@@ -1,18 +1,17 @@
 # CS 534
 # AI1 skeleton code
 # By Quintin Pope
-import csv
-import copy
-#import numpy as np
-
+import pandas as pd
+import numpy as np
 
 # Loads a data file from a provided file location.
 def load_data(path):
     # Your code here:
-    with open(path, 'r') as input:
-        read_data = csv.reader(input)
-        loaded_data = [row for row in read_data]
-        return loaded_data
+    loaded_data = pd.read_csv(path)
+    #print(data_train)
+    return loaded_data
+    
+
 
 # Implements dataset preprocessing, with boolean options to either normalize the data or not, 
 # and to either drop the sqrt_living15 column or not.
@@ -21,24 +20,40 @@ def load_data(path):
 # / aren't normalized, or versions that have / lack sqrt_living15.
 def preprocess_data(data, normalize, drop_sqrt_living15):
     # Your code here:
-    preprocessed_data = data.copy()
-    preprocessed_data[0][14] = 'age_since_renovated'
-    for i in range(1, len(data)):
-        year = float(data[i][1].split('/')[2])
-        month = float(data[i][1].split('/')[0])
-        date = float(data[i][1].split('/')[1])
-        preprocessed_data[i][14] = year - float(preprocessed_data[i][13]) if float(preprocessed_data[i][14]) == 0 else year - float(preprocessed_data[i][14])
-        preprocessed_data[i] = [1] + [year] + [month] + [date] + preprocessed_data[i][2:]
-        preprocessed_data[i] = [float(n) for n in preprocessed_data[i]]
-        print(preprocessed_data[i])
+    #drop id
+    data.drop(columns=['id'], inplace=True)
 
-    if not normalize:
-        for i in range(len(data[1])):
-            col = list()
+    #split the date and drop data value
+    data = pd.merge((data['date'].str.split('/', expand = True)), data,
+    how='left', left_index= True, right_index= True)
+    
+    data.drop(columns=['date'], inplace=True)
+    data.rename(columns={0:'month', 1:'day', 2:'year'}, inplace=True)
+    data[['month', 'day', 'year']] = data[['month', 'day', 'year']].astype('int')
+    data.insert(loc=0, column='dummy', value=1)
+    
+    #change renovate year
+    for i in range(data.shape[0]):
+        if data['yr_renovated'][i] == 0:
+            data['yr_renovated'][i] = data['year'][i] - data['yr_built'][i]
+        else:
+            data['yr_renovated'][i] = data['year'][i] - data['yr_renovated'][i]
+    
+    data.rename(columns={'yr_renovated':'age_science_renovated'}, inplace=True)
+    
+    #normalize
+    if normalize == False:
+        features = [f for f in data.columns if f not in ['dummy', 'waterfront', 'price']]
+        for feature in features:
+            current_mean = data[feature].mean()
+            current_std = data[feature].std()
+            for i in range(data.shape[0]):
+                data[feature][i] = (data[feature][i] - current_mean)/current_std
+    
+    normalize = True
 
-            for j in range(1, len(data)):
-            col = [data[j][i]
 
+    preprocessed_data = data
     return preprocessed_data
 
 # Implements the feature engineering required for part 4. Quite similar to preprocess_data.
@@ -68,10 +83,10 @@ def plot_losses(losses):
 
 # Part 0  : Data preprocessing.
 # Your code here:
-training_data = load_data('IA1_train.csv')
-valid_data = load_data('IA1_dev.csv')
-
-preprocess_data(training_data, False, False)
+d = load_data('IA1_train.csv')
+print(d.info())
+f = preprocess_data(d,False,1)
+print(f.head(10))
 
 # Part 1 . Implement batch gradient descent and experiment with different learning rates.
 # Your code here:
